3c69413c478efe653740512571a2417f8306c8a8
quoctrieu010903 <luongquoctrieu165@gmail.com>
2024-06-07 11:48:56 +0700

    update main code to see green foreveragin

--- src/main/java/com/quoctrieu/mthutil/mathutil/core/MathUtil1808.java
+++ src/main/java/com/quoctrieu/mthutil/mathutil/core/MathUtil1808.java
@@ -34,7 +34,8 @@ public class MathUtil1808 {
     //n = 0...20! vì n = 21! vượt 10 số 0, 18 sô là vừa dủ long
     public static long getFactorial(int n){
         
-        long product = 10; //tích - kết quả phép nhân
+        long product = 1; //tích - kết quả phép nhân
+        //update code again to see green 4er
         if(n < 0 || n > 20 )
             throw new IllegalArgumentException("Invalid n , n must be between 0 to 20!!");
         if(n == 0 )
